57db3cc21d9130e5446c5e4988ba1a80aa10b673
Nick West <mail@nickwe.st>
2016-07-11 00:37:44 -0400

    spotify undefined search result fixed

--- chiri.js
+++ chiri.js
@@ -7,9 +7,27 @@ var request = require('request');
 var logData = function(logEntry) {
     fs.appendFile("./log.txt", logEntry, (error) => {
         if (error) {
-            throw error;
+          console.log(error);
         }
     });
+}
+var slice = process.argv.slice(3);
+var songSearch = function(songQuery){
+  var songData;
+  var song;
+  spotify.search(
+      {type: 'track', query: songQuery},
+      function(error, data){
+        if(!error){
+          song = data.tracks.items[0];
+          songData = ("Artist: " + song.artists[0].name + "\n" + "Song title: " + song.name + "\n" + "Album: " + song.album.name + "\n" + "Preview: " + song.preview_url + "\n");
+          console.log(songData);
+          logData(songData);
+        } else if (error){
+          console.log(error);
+        }
+      }
+    )
 }
 
 switch(process.argv[2]){
@@ -29,17 +47,19 @@ switch(process.argv[2]){
         record = (tweets[i].text + "\n Tweeted at: " + tweets[i].created_at + "\n")
         console.log(record);
         logData("my-tweets at " + Date() + "\n" + record + "---END ENTRY---");
-        // record = (tweets);
-        // for (var i = 0; i < 20; i++){
-        //   console.log(record[i].text);
-        //   logData(record);  
         }            
       }else if (error){ 
         console.log(error);
       }
     });
   case "spotify-this-song":
-//end switch    
+    if(toString(slice) === '[object Undefined]'){
+      songSearch("What's my age again");
+    }
+    else{
+      songSearch(slice);
+    }
+//end switch     
 }
 
 // } else if(process.argv[2] == "spotify-this-song"){
