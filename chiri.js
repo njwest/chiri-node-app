5fb63f409c8618f217d835ab3b8458a64993dcf4
Nick West <mail@nickwe.st>
2016-07-10 23:29:59 -0400

    log function added, twitter cleaned

---
+++ chiri.js
@@ -0,0 +1,58 @@
+var keys = require('./keys.js');
+var fs = require('fs');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+var logData = function(logEntry) {
+    fs.appendFile("./log.txt", logEntry, (error) => {
+        if (error) {
+            throw error;
+        }
+    });
+}
+
+switch(process.argv[2]){
+  case "my-tweets":
+    var client = new twitter({
+      consumer_key: keys.twitterKeys.consumer_key,
+      consumer_secret: keys.twitterKeys.consumer_secret,
+      access_token_key: keys.twitterKeys.access_token_key,
+      access_token_secret: keys.twitterKeys.access_token_secret
+    });
+    var me = 'n1ckw3st';
+    var record;
+    var params = {screen_name: 'n1ckw3st'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+      if (!error){
+        for (var i = 0; i < tweets.length; i++){
+        record = (tweets[i].text + "\n Tweeted at: " + tweets[i].created_at + "\n")
+        console.log(record);
+        logData("my-tweets at " + Date() + "\n" + record + "---END ENTRY---");
+        // record = (tweets);
+        // for (var i = 0; i < 20; i++){
+        //   console.log(record[i].text);
+        //   logData(record);  
+        }            
+      }else if (error){ 
+        console.log(error);
+      }
+    });
+  case "spotify-this-song":
+//end switch    
+}
+
+// } else if(process.argv[2] == "spotify-this-song"){
+//   console.log("lol");
+// // console log song artist, song name, preview link of song, album of song, song name
+// // output what's my age again by blink 182 if process.argv[3] is undefined
+// } else if(process.argv[2] == "movie-this"){
+//     console.log("lol");
+
+//   // console.log title, year, imdb rating, country, language, plot, actors
+//   // console.log Mr Nobody movie info if process.argv[3] is undefined
+// } else if(process.argv[2] == "do-what-it-says"){
+//     console.log("lol");
+
+//   // take in the text from random.txt and use it as "node liri.js [contents 1 = process.argv[2] [contents 2 = process.argv[3]"
+// }
